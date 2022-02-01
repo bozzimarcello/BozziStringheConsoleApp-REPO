af5d40398f21fbc3bc11222875b7bf50abc29d83
Marcello Bozzi <bozzimarcello@gmail.com>
2022-02-01 08:57:47 +0100

    Aggiunta dei file al progetto

---
+++ BozziStringheConsoleApp/BozziStringheConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BozziStringheConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stringhe Console App");
+            Console.WriteLine("Questo programma analizza una frase, lettera per lettera");
+
+            Console.WriteLine("Inserisci una frase:");
+            string frase = Console.ReadLine();
+
+            for(int i = 0; i<frase.Length; i++)
+            {
+                char carattere = frase[i];
+                Console.WriteLine(carattere);
+            }
+        }
+    }
+}
